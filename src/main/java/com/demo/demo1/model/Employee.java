cf47cfbc54b4383084f426e52410431401bb807f
DevanjanMahanti <devanjanmahanti4@gmail.com>
2022-10-16 13:16:17 +0530

    lombok annotations

--- src/main/java/com/demo/demo1/model/Employee.java
+++ src/main/java/com/demo/demo1/model/Employee.java
@@ -1,7 +1,19 @@
 package com.demo.demo1.model;
 
+import lombok.*;
 import org.springframework.stereotype.Component;
 
 @Component
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Employee {
+
+    private String name;
+
+    private Integer Id;
+
 }

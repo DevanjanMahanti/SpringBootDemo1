0a6a619701d191b9c2b8251157ce2188f57db7cd
DevanjanMahanti <devanjanmahanti4@gmail.com>
2022-10-16 13:46:35 +0530

    adding get api

--- src/main/java/com/demo/demo1/service/EmployeeService.java
+++ src/main/java/com/demo/demo1/service/EmployeeService.java
@@ -1,8 +1,13 @@
 package com.demo.demo1.service;
 
 
+import com.demo.demo1.model.Employee;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
+    public Employee getEmployeeById(Integer id) {
+        Employee emp = new Employee("diksha",123);
+        return emp;
+    }
 }
